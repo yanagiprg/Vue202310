5e06924ab2d8555fe7312e1443bd7ff392f2fef9
takayanagi shotaro <s.takayanagi1116@gmail.com>
2023-10-10 10:08:50 +0900

    コメント機能を追加

--- src/components/PostDetail.vue
+++ src/components/PostDetail.vue
@@ -4,7 +4,7 @@
       <h2 class="text-2xl font-semibold mb-4">{{ post.title }}</h2>
       <p class="text-gray-700">{{ post.content }}</p>
       <p class="text-gray-500 text-sm mt-2">作成者: {{ post.userName }}</p>
-      <p class="text-gray-500 text-sm mt-2">
+      <p v-if="post.updatedAt" class="text-gray-500 text-sm mt-2">
         更新時間: {{ formatTimestamp(post.updatedAt) }}
       </p>
       <router-link
@@ -22,32 +22,113 @@
         削除
       </button>
     </div>
+    <!-- Comments Section -->
+    <div v-if="comments?.length" class="mt-10 max-w-2xl mx-auto">
+      <h3 class="text-xl font-semibold mb-4 border-b-2 border-gray-300 pb-2">
+        コメント
+      </h3>
+      <div
+        v-for="comment in comments"
+        :key="comment.id"
+        class="bg-white p-4 mb-6 rounded-lg shadow relative"
+      >
+        <div
+          v-if="authUser && authUser.uid === comment.userId"
+          class="absolute top-2 right-2"
+        >
+          <button
+            @click.prevent="removeComment(comment.id)"
+            class="text-red-500 hover:text-red-600"
+          >
+            <span class="material-icons">delete</span>
+          </button>
+        </div>
+
+        <p class="text-gray-700 mb-2 border-b pb-2">{{ comment.content }}</p>
+        <div class="flex justify-between items-center mt-2">
+          <div class="flex">
+            <span class="material-icons text-gray-500 mr-2">ユーザ名：</span>
+            <p class="text-gray-500 text-sm mr-4">{{ comment.userName }}</p>
+          </div>
+          <p class="text-gray-500 text-sm">
+            {{ formatJapaneseDate(comment.createdAt) }}
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <!-- Add Comment Form -->
+    <div
+      v-if="authUser"
+      class="mt-10 max-w-2xl mx-auto p-4 bg-white rounded shadow"
+    >
+      <textarea
+        v-model="newComment"
+        class="w-full p-2 rounded mb-4 border"
+        placeholder="コメントを入力..."
+      ></textarea>
+      <button
+        @click.prevent="submitComment"
+        class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 focus:outline-none"
+      >
+        コメントする
+      </button>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { mapState, mapActions } from "vuex";
-import { formatTimestamp } from "@/utils/formatTimestamp";
+import { formatTimestamp, formatJapaneseDate } from "@/utils/formatTimestamp";
 
 export default {
   props: {
     authUser: Object,
   },
+  data() {
+    return {
+      newComment: "",
+    };
+  },
   async created() {
     const postId = this.$route.params.id;
     await this.getPostById(postId);
+    await this.getComments(postId);
   },
   computed: {
-    ...mapState("posts", ["post"]),
+    ...mapState("posts", ["post", "comments"]),
   },
   methods: {
-    ...mapActions("posts", ["getPostById", "removePost"]),
+    ...mapActions("posts", [
+      "getPostById",
+      "removePost",
+      "getComments",
+      "addComment",
+      "deleteComment",
+    ]),
     async deletePost() {
       const postId = this.$route.params.id;
       await this.removePost(postId);
       this.$router.push("/");
     },
+    submitComment() {
+      if (!this.newComment.trim()) return;
+
+      const comment = {
+        content: this.newComment,
+        userId: this.authUser.uid,
+        userName: this.authUser.displayName,
+        createdAt: new Date().toISOString(),
+      };
+
+      this.addComment({ postId: this.$route.params.id, comment });
+      this.newComment = "";
+    },
+    removeComment(commentId) {
+      this.deleteComment({ postId: this.$route.params.id, commentId });
+    },
     formatTimestamp,
+    formatJapaneseDate,
   },
 };
 </script>
